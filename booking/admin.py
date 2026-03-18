5d426cb299f2c018e406d770e61877c0e32135d3
Admin <Admin@atyrau.itstep>
2026-03-18 21:27:14 +0500

    for_students

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Table, Reservation
 
-# Register your models here.
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ("id", "number", "x", "y")
+    search_fields = ("number",)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "table", "data", "time", "created")
+    list_filter = ("data", "table")
